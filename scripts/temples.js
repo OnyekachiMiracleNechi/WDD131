d95b18363b115030d325acb880d2be34cc6e671e
OnyekachiMiracleNechi <168313634+OnyekachiMiracleNechi@users.noreply.github.com>
2024-10-16 19:28:59 +0100

    new commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,13 +2,15 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
 
 
-document.addEventListener('DOMContentLoaded', function() {
+
+
+document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.getElementById('hamburger');
     const navMenu = document.getElementById('navMenu');
 
+    hamburger.addEventListener('click', function () {
+        navMenu.classList.toggle('active'); // Toggle the 'active' class
+        hamburger.textContent = navMenu.classList.contains('active') ? '✖' : '☰'; // Change to "X" or hamburger
+    });
+});
 
-    // Toggle the navigation menu
-    hamburger.addEventListener('click', function() {
-        navMenu.style.display = (navMenu.style.display === 'block') ? 'none' : 'block';
-    }
-    )});
