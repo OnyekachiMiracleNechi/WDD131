80f784b9f97941500c02ed0d951d7f4c078a5c3b
OnyekachiMiracleNechi <168313634+OnyekachiMiracleNechi@users.noreply.github.com>
2024-10-29 23:36:40 +0100

    new commit

--- scripts/project.js
+++ scripts/project.js
@@ -1,13 +1,23 @@
-
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('navMenu');
+    const navMenu = document.querySelector('nav ul');
 
     hamburger.addEventListener('click', function () {
-        navMenu.classList.toggle('active'); // Toggle the 'active' class
-        hamburger.textContent = navMenu.classList.contains('active') ? '✖' : '☰'; // Change to "X" or hamburger
+        navMenu.classList.toggle('active');
+        hamburger.textContent = navMenu.classList.contains('active') ? '✖' : '☰'; // Change to "X" or hamburgers
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.getElementById('cta-button');
+    
+    ctaButton.addEventListener('click', function() {
+        // Navigate to the services page
+        window.location.href = 'services.html';
+    });
+});
+
+
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
+
